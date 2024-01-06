3cb338b99d2493c1e8ab008f5148a7ffeceddd49
liu <normidar7@gmail.com>
2024-01-06 15:22:33 +0900

    Update buying text format in sim_companies_example.dart

--- example/sim_companies_example.dart
+++ example/sim_companies_example.dart
@@ -4,7 +4,8 @@ void main() {
   final product = Samosa(quantity: d(103 * 8), quality: 2);
   final buyingTextGenerator = BuyingTextGenerator(product);
   final text = buyingTextGenerator.generateBuyingToProduct(
-    format: "&imag q&qual &quanunit @-2%mp",
+    header: 'Buying: 買: 买: @-2%mp',
+    format: "&imag q&qual &quanunit",
   );
   print(text);
 }
